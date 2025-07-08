5e4e366d9bd5c8bededace02f642cc2083f6f3a2
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-08 18:21:55 +0200

    feat: 4 excercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,48 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const vocals = (element) => {
+
+  const vocali = ["a", "e", "i", "o", "u"]
+
+  //contatore 
+  let contatore = 0 ;
+
+  for( let i = 0 ; i < element.length ; i++) {
+
+    if(vocali.includes(element[i])){
+      contatore++
+    }
+  }
+
+  return contatore;
+
+}
 
 
-// Invoca la funzione qui e stampa il risultato in console
 
+// // Invoca la funzione qui e stampa il risultato in console
 
+console.log(vocals(word))
 
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+// //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+const words = 'javascript';
+
+const consonante = (element) => {
+
+  const consonant = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+  let contatore = 0; 
+
+  for(let i = 0 ; i<element.length ; i++) {
+
+    if(consonant.includes(element[i])) {
+      contatore++
+    }
+  }
+
+  return contatore
+}
+
+console.log(consonante(words))
